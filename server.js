1536cc808c901e1c573993a29d5dc9e37d7e8677
Uday yadav <119792326+Udayydv@users.noreply.github.com>
2024-03-01 13:42:49 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,7 @@
+console.log("hello world")
+
+/*We can re install the node modules by typing
+  (npm install example: slugify) */
+
+/*Here we are using stand alone java script as
+  we are using it as back-end javascript*/
