572cf1eeeb67a63d405a62517fffcac4c70787c4
Uday yadav <119792326+Udayydv@users.noreply.github.com>
2024-03-14 14:22:39 +0530

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-var fs = require("fs");
-console.log("Going to open a file");
-fs.open('input.txt','r+',function(err,fd){
-    if(err){
-        return console.error(err);
-    }
-    console.log("file opened succesfully");
-});
